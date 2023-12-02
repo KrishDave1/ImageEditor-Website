dee34d4c0cce0d240a2a018f513130e205a2934f
valmikGit <quantumcomputingiiitb@gmail.com>
2023-12-02 16:49:39 +0530

    Minor edit.

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/libraryInterfaces/BrightnessInterface.java
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/libraryInterfaces/BrightnessInterface.java
@@ -3,7 +3,8 @@ package com.iiitb.imageEffectApplication.libraryInterfaces;
 public class BrightnessInterface {
     static {
         String libraryPath = "BrightnessLib";
-        LoadNativeLibrary.loadNativeLibrary(libraryPath);
+//        LoadNativeLibrary.loadNativeLibrary(libraryPath);
+        LoadNativeLibrary.loadNativeLibrary(libraryPath + ".so");
     }
     public static native Pixel[][] applyBrightness(Pixel[][] image, float amount);
 }
