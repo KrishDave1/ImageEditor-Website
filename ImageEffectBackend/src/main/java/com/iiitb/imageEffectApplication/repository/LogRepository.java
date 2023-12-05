ceb630a330da9f36e8795948b0b24f86ed9adf5a
valmikGit <quantumcomputingiiitb@gmail.com>
2023-12-06 00:43:48 +0530

    Minor edits.

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/repository/LogRepository.java
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/repository/LogRepository.java
@@ -3,4 +3,5 @@ import com.iiitb.imageEffectApplication.model.LogModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface LogRepository extends MongoRepository {
+
 }
