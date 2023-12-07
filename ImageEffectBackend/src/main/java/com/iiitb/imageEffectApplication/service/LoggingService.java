4a2592f78193ed9b42532bab41a73600a1fcc971
valmikGit <quantumcomputingiiitb@gmail.com>
2023-12-07 22:34:16 +0530

    Created basic REST api.

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/service/LoggingService.java
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/service/LoggingService.java
@@ -1,5 +1,7 @@
 package com.iiitb.imageEffectApplication.service;
 import com.iiitb.imageEffectApplication.model.LogModel;
+import com.iiitb.imageEffectApplication.repository.LogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -11,7 +13,8 @@ import java.util.stream.Collectors;
 
 @Service
 public class LoggingService {
-
+    @Autowired
+    private LogRepository logRepository;
     private List<LogModel> logs = new ArrayList<>();
     public void addLog(String fileName, String effectName, String optionValues) {
         // Get the current date and time
@@ -21,21 +24,24 @@ public class LoggingService {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         String formattedTime = currentTime.format(formatter);
         LogModel logModel = new LogModel(formattedTime, fileName, effectName, optionValues);
-        logs.add(logModel);
+//        logs.add(logModel);
+        logRepository.save(logModel);
     }
 
     public List<LogModel> getAllLogs() {
-        return logs;
+        return logRepository.findAll();
+//        return logs;
     }
 
     public List<LogModel> getLogsByEffect(String effectName) {
-        List<LogModel> logModels = new ArrayList<LogModel>();
-        for (LogModel logModel : logs) {
-            if (Objects.equals(effectName, logModel.getEffectName())) {
-                logModels.add(logModel);
-            }
-        }
-        return logModels;
+//        List<LogModel> logModels = new ArrayList<LogModel>();
+//        for (LogModel logModel : logs) {
+//            if (Objects.equals(effectName, logModel.getEffectName())) {
+//                logModels.add(logModel);
+//            }
+//        }
+//        return logModels;
+        return logRepository.findLogByEffectName(effectName);
     }
 
     public void clearLogs() {
