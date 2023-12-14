8b7e8de3e502bf9b81de0dc031b92c27d7aaf483
valmikGit <quantumcomputingiiitb@gmail.com>
2023-12-15 01:09:55 +0530

    Deleted LogRepositry.java.

--- ImageEffectBackend/src/main/java/com/iiitb/LogRepositry.java
+++
@@ -1,9 +0,0 @@
-package com.iiitb;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import com.iiitb.imageEffectApplication.model.LogModel;
-
-public interface LogRepositry extends MongoRepository<LogModel, String> {
-    
-}
-
