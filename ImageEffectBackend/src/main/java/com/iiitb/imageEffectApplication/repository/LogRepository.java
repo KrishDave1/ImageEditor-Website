79f95f45306eba71100dd20a8930d5afd391c2a6
valmikGit <quantumcomputingiiitb@gmail.com>
2023-12-30 14:51:05 +0530

    Added documentation to the LogRepository.java file

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/repository/LogRepository.java
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/repository/LogRepository.java
@@ -11,3 +11,7 @@ public interface LogRepository extends MongoRepository<LogModel, String> {
     @Query(value = "{effectName : '?0'}", fields = "{'timestamp' :  1, 'filename' :  1, 'effectName' :  1, 'optionValues' : 1}")
     List<LogModel> findLogByEffectName(String effect);
 }
+
+//this code declares a Spring Data MongoDB repository interface (LogRepository) for the LogModel entity.
+// It includes a custom query method (findLogByEffectName) with a MongoDB query that searches for logs based on
+// the 'effectName' field and specifies the fields to be included in the result.
